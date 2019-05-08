01b5dc40437f0ddf4e6de4542e3eceb6001c25a9
GET Academy <terje@kolderup.net>
2019-05-08 10:21:10 +0200

    x

--- WavingMan/Program.cs
+++ WavingMan/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WavingMan
@@ -10,6 +11,15 @@ namespace WavingMan
     {
         static void Main(string[] args)
         {
+            var stage = new Stage();
+
+            while (true)
+            {
+                stage.ShowAndMove();
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                Thread.Sleep(200);
+            }
         }
     }
 }
